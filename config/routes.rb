4c7af784b03e63048ee3639e4e2f4c8d7757782f
Andrew Baardson <andrewbaardson@gmail.com>
2019-02-26 18:43:23 -0700

    working commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+  get 'appointments/index'
+  get 'appointments/new'
+  get 'appointment/index'
+  get 'appointment/new'
+  root 'doctors#index'
+
+  resources :users
+  resources :doctors do
+    resources :appointments, only: [:index, :new, :create, :destroy]
+  end
+  end
